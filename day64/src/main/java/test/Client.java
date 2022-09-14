ce0f0ec805755f9330d3d208eda2e6e467b6452c
tahiti8505 <tahiti8505@naver.com>
2022-09-15 07:32:15 +0900

    constructor injection, setter injection, DI

--- day64/src/main/java/test/Client.java
+++ day64/src/main/java/test/Client.java
@@ -9,6 +9,7 @@ public class Client {
 	public static void main(String[] args) {
 		// Spring 컨테이너를 동작시킬 수 있도록 코드 작성
 	    AbstractApplicationContext factory=new GenericXmlApplicationContext("applicationContext.xml");
+	    System.out.println("로그");
 	    Phone phone = (Phone)factory.getBean("ip");
 	    phone.volumeDown();
 	    factory.close();
