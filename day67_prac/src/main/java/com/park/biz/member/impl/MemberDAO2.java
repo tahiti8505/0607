3887a5d013a5e641a1ac1ac3d5375f4ec647fd24
tahiti8505 <tahiti8505@naver.com>
2022-09-19 21:16:54 +0900

    0919

---
+++ day67_prac/src/main/java/com/park/biz/member/impl/MemberDAO2.java
@@ -0,0 +1,55 @@
+package com.park.biz.member.impl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.park.biz.member.MemberVO;
+
+@Repository("memberDAO")
+public class MemberDAO2 {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	final String sql_selectOne="SELECT * FROM MEMBER WHERE MID=? AND MPW=?";
+	final String sql_selectAll="SELECT * FROM MEMBER";
+	final String sql_insert="INSERT INTO MEMBER VALUES(?,?,?,?)";
+	final String sql_update="UPDATE MEMBER SET MPW=? WHERE MID=?";
+	final String sql_delete="DELETE MEMBER WHERE MID=? AND MPW=?";
+	
+	public void insertMember(MemberVO vo) {
+		jdbcTemplate.update(sql_insert,vo.getMid(),vo.getMpw(),vo.getName(),vo.getRole());
+	}
+	public void deleteMember(MemberVO vo) {
+		jdbcTemplate.update(sql_delete,vo.getMid(),vo.getMpw());
+	}
+	public void updateMember(MemberVO vo) {
+		jdbcTemplate.update(sql_update,vo.getMpw(),vo.getMid());
+	}
+	public MemberVO selectOneMember(MemberVO vo) {
+		Object[] args= {vo.getMid(),vo.getMpw()};
+		return jdbcTemplate.queryForObject(sql_selectOne,args,new MemberRowMapper());
+	}
+	public List<MemberVO> selectAllMember(MemberVO vo) {
+		return jdbcTemplate.query(sql_selectAll,new MemberRowMapper());
+	}
+}
+class MemberRowMapper implements RowMapper<MemberVO> {
+
+	@Override
+	public MemberVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		MemberVO data=new MemberVO();
+		data.setMid(rs.getString("MID"));
+		data.setMpw(rs.getString("MPW"));
+		data.setName(rs.getString("NAME"));
+		data.setRole(rs.getString("ROLE"));
+		return data;
+	}
+	
+}
---
+++ day67_prac/src/main/java/com/park/biz/member/impl/MemberServiceImpl.java
@@ -0,0 +1,45 @@
+package com.park.biz.member.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.park.biz.member.MemberService;
+import com.park.biz.member.MemberVO;
+
+@Service("memberService")
+public class MemberServiceImpl implements MemberService {
+
+	@Autowired // MemberDAO Ÿ���� ��ü�� �޸𸮿� �־������ DI(������ ����) ������!
+	private MemberDAO2 memberDAO; // �ٽɷ����� ������ ��ü
+	
+	@Override
+	public void insertMember(MemberVO vo) {
+		memberDAO.insertMember(vo);
+	}
+
+	@Override
+	public void deleteMember(MemberVO vo) {
+		memberDAO.deleteMember(vo);
+	}
+
+	@Override
+	public void updateMember(MemberVO vo) {
+		memberDAO.updateMember(vo);
+	}
+
+	@Override
+	public MemberVO selectOneMember(MemberVO vo) {
+		if(vo.getMid().equals("timo")) {
+			throw new IllegalArgumentException("[����ÿ���]");
+		}
+		return memberDAO.selectOneMember(vo);
+	}
+
+	@Override
+	public List<MemberVO> selectAllMember(MemberVO vo) {
+		return memberDAO.selectAllMember(vo);
+	}
+
+}
